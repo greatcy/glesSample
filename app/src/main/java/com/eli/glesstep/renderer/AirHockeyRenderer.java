3549f968267675465596ef33af2781f160de6243
eli <1525843015@qq.com>
2017-09-03 22:01:44 +0800

    添加3D处理

--- app/src/main/java/com/eli/glesstep/renderer/AirHockeyRenderer.java
+++ app/src/main/java/com/eli/glesstep/renderer/AirHockeyRenderer.java
@@ -33,11 +33,13 @@ import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 import android.content.Context;
+import android.opengl.GLES20;
 import android.opengl.GLSurfaceView.Renderer;
 import android.opengl.Matrix;
 
 import com.eli.glesstep.LoggerConfig;
 import com.eli.glesstep.R;
+import com.eli.glesstep.utils.MatrixHelper;
 import com.eli.glesstep.utils.ShaderHelper;
 import com.eli.glesstep.utils.TextResourceReader;
 
@@ -45,7 +47,7 @@ public class AirHockeyRenderer implements Renderer {
     private static final String A_POSITION = "a_Position";
     private static final String A_COLOR = "a_Color";
     private static final String U_MATRIX = "u_Matrix";
-    private static final int POSITION_COMPONENT_COUNT = 4;
+    private static final int POSITION_COMPONENT_COUNT = 2;
     private static final int COLOR_COMPONENT_COUNT = 3;
     private static final int BYTES_PER_FLOAT = 4;
     private static final int STRIDE =
@@ -57,37 +59,35 @@ public class AirHockeyRenderer implements Renderer {
     private int program;
     private int aPositionLocation;
     private int aColorLocation;
+    private final float modelMatrix[] = new float[16];
+
     private int uMatrixLocation;
 
+    //1、正交投影矩阵,用来适配到不同屏幕上,保持比例一致
+    //2、透视投影生成的矩阵，用来实现透视投影的效果
     private final float[] projectMatrix = new float[16];
 
     public AirHockeyRenderer(Context context) {
         this.context = context;
 
-        //
-        // Vertex data is stored in the following manner:
-        //
-        // The first two numbers are part of the position: X, Y
-        // The next three numbers are part of the color: R, G, B
-        //
         float[] tableVerticesWithTriangles = {
                 // Order of coordinates: X, Y, R, G, B
 
                 // Triangle Fan
-                0f, 0f, 0f, 1.5f, 1f, 1f, 1f,
-                -0.5f, -0.8f, 0f, 1f, 0.7f, 0.7f, 0.7f,
-                0.5f, -0.8f, 0f, 1f, 0.7f, 0.7f, 0.7f,
-                0.5f, 0.8f, 0f, 2f, 0.7f, 0.7f, 0.7f,
-                -0.5f, 0.8f, 0f, 2f, 0.7f, 0.7f, 0.7f,
-                -0.5f, -0.8f, 0f, 1f, 0.7f, 0.7f, 0.7f,
+                0f, 0f, 1f, 1f, 1f,
+                -0.5f, -0.8f, 0.7f, 0.7f, 0.7f,
+                0.5f, -0.8f, 0.7f, 0.7f, 0.7f,
+                0.5f, 0.8f, 0.7f, 0.7f, 0.7f,
+                -0.5f, 0.8f, 0.7f, 0.7f, 0.7f,
+                -0.5f, -0.8f, 0.7f, 0.7f, 0.7f,
 
                 // Line 1
-                -0.5f, 0f, 0f, 1.5f, 1f, 0f, 0f,
-                0.5f, 0f, 0f, 1.5f, 1f, 0f, 0f,
+                -0.5f, 0f, 1f, 0f, 0f,
+                0.5f, 0f, 1f, 0f, 0f,
 
                 // Mallets
-                0f, -0.4f, 0f, 1.25f, 0f, 0f, 1f,
-                0f, 0.4f, 0f, 1.75f, 1f, 0f, 0f
+                0f, -0.4f, 0f, 0f, 1f,
+                0f, 0.4f, 1f, 0f, 0f
         };
 
         vertexData = ByteBuffer
@@ -153,18 +153,37 @@ public class AirHockeyRenderer implements Renderer {
         // Set the OpenGL viewport to fill the entire surface.
         glViewport(0, 0, width, height);
 
-        final float aspectRatio = width > height ?
-                (float) width / (float) height :
-                (float) height / (float) width;
+//        final float aspectRatio = width > height ?
+//                (float) width / (float) height :
+//                (float) height / (float) width;
+//
+//        //获得矩阵存储到数组
+//        if (width > height) {
+//            //landscape
+//            Matrix.orthoM(projectMatrix, 0, -aspectRatio, aspectRatio, -1, 1, -1, 1);
+//        } else {
+//            //portrait
+//            Matrix.orthoM(projectMatrix, 0, -1, 1, -aspectRatio, aspectRatio, -1, 1);
+//        }
 
-        //获得矩阵存储到数组
-        if (width > height) {
-            //landscape
-            Matrix.orthoM(projectMatrix, 0, -aspectRatio, aspectRatio, -1, 1, -1, 1);
-        } else {
-            //portrait
-            Matrix.orthoM(projectMatrix, 0, -1, 1, -aspectRatio, aspectRatio, -1, 1);
-        }
+        //使用透视投影的方法
+        MatrixHelper.perspectiveM(projectMatrix,
+                45, (float) width / (float) height, 1f, 10f);
+
+        //设置平移矩阵
+        Matrix.setIdentityM(modelMatrix, 0);
+
+        //旋转桌子
+        Matrix.translateM(modelMatrix, 0, 0f, 0f, -2.5f);
+        Matrix.rotateM(modelMatrix, 0, -60f, 1f, 0f, 0f);
+
+        //合并透视和平移矩阵
+        final float temp[] = new float[16];
+        Matrix.multiplyMM(temp, 0, projectMatrix, 0,
+                modelMatrix, 0);
+        System.arraycopy(temp, 0, projectMatrix,
+                0, temp.length);
+
     }
 
     /**
